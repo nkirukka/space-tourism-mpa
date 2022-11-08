6640a79c2e255f49fe9b31a65e295319a62b0b9b
Nkiruka Ebele <specialebele@gmail.com>
2022-11-08 23:02:25 +0100

    feat: update page with details

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import '../styles/Destination.css';
+import {data} from '../starter-code/data.js';
+import { useState } from 'react';
 
 const Destination = () => {
+  const [places] = useState(data.destinations);
+  const [value, setValue] = useState(0);
+
+  const { name, images, description, distance, travel } = places[value];
+
   return (
-    <div>Destination</div>
-  )
+    <section className='Destination'>
+      <img src={images.png} alt={name} />
+
+      <article className='Destination__details'>
+        <div>
+          {places.map((item, idx) => {
+            return <button onClick={()=>{setValue(idx)}}>{ item.name}</button>
+          })}
+        </div>
+        
+        <h2>{name}</h2>
+        <p>{description}</p>
+        
+        <article className='Avg-est'>
+
+          {/* Average */}
+          <div>
+            <p>AVG. DISTANCE</p>
+            <span>{distance}</span>
+          </div>
+
+          {/* Distance */}
+          <div>
+            <p>EST. TRAVEL TIME</p>
+            <span>{travel}</span>
+          </div>
+        </article>
+      </article>
+    </section>
+  );
 }
 
-export default Destination
+export default Destination;
