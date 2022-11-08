e759afbf41f6a63425a792a7b6e7e7aa8da16658
Nkiruka Ebele <specialebele@gmail.com>
2022-11-08 13:40:33 +0100

    feat: add inner class to space div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,15 @@ const Home = () => {
     <main className='Home'>
       
       <div className='Space'>
-        <div>
+        <div className='Space-inner'>
           <h1 className='ff-barlow_cond'> SO, YOU WANT TO TRAVEL TO <span className='ff-bellefair'>SPACE</span></h1>
           <p className='ff-barlow'>Let's face it; if you want to go to space, you might as well genuinely go to
             outer space and not hover kind of on the edge of it. Well sit back, and relax
             because we'll give you a truly out of this world experience!</p>
         </div>
+
         <div className='Explore'>
-        <Link to='/destination' className='ff-bellefair'>EXPLORE</Link>
+          <Link to='/destination' className='ff-bellefair'>EXPLORE</Link>
         </div>
       </div>
     </main>
