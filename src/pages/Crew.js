58410459a9f2d88a799100cf8ee0ae1495966933
Nkiruka Ebele <specialebele@gmail.com>
2022-11-16 19:46:51 +0100

    feat: create complete component for crew route

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import '../styles/Crew.css';
+import { data } from "../starter-code/data";
 
 const Crew = () => {
+  const [people] = useState(data.crew);
+  const [value, setValue] = useState(0);
+  const [active, setActive] = useState(null);
+
+  const {
+    name,
+    images,
+    bio,
+    role } = people[value];
+
+  const btns = [
+    {id: 0},
+    {id: 1},
+    {id: 2},
+    {id: 3},
+  ]
   return (
-    <div>Crew</div>
+    <section className='Crew'>
+    <div className='Crew__wrapper'>
+      <div className='People'>
+        <h2 className='ff-barlow_cond'><span>02</span> MEET YOUR CREW</h2>
+          <img
+            src={images.png}
+            alt={name}
+            title={name}
+            className='Crew__img'
+          />
+      </div>
+    
+      <article className='Crew__details'>
+        <div className='Crew__buttons'>
+            {people.map((item, idx) => {
+            return <button
+              type='button'
+              className={`ff-barlow_cond ${active === idx ? 'active' : null}`}
+              key={Math.random()}
+              onClick={() => { setValue(idx); setActive(btns[idx].id) }}
+            >
+            </button>
+          })}
+        </div>
+    
+          <p className="ff-bellefair">{role.toUpperCase() }</p>
+        <h3 className='ff-bellefair'>{name.toUpperCase()}</h3>
+        <p className='ff-barlow Bio'>{bio}</p>
+        
+      </article>
+
+    </div>
+  </section>
   )
 }
 
